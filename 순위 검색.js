857652fa65a45c5da82e5a399980037cd439d256
unknown <dsy0302@gmail.com>
2022-06-30 17:45:54 +0900

    오늘 푼문제

--- 순위 검색.js
+++ 순위 검색.js
@@ -1,40 +1,40 @@
+// 정확성은 통과했으나 효율성은 통과하지 못함
 function solution(info, query) {
-  const infoArr = [];
-  const queryArr = [];
-  const answer = [];
+	const infoArr = [];
+	const queryArr = [];
+	const answer = [];
 
-  info.forEach((el) => infoArr.push(el.split(" ")));
-  query.forEach((el) => {
-    const splited = el.split(" and ");
-    splited.push(...splited.pop().split(" "));
-    queryArr.push(splited);
-  });
-  console.log(infoArr);
-  console.log(queryArr);
-
-  queryArr.forEach((query) => {
-    let total = 0;
-    console.log(query);
-    infoArr.forEach((info) => {
-      let cnt = 0;
-      console.log(info);
-      for (let i = 0; i < 5; i++) {
-        console.log(query[i]);
-        console.log(info[i]);
-        if (i === 4) {
-          cnt = query[i] === info[i];
-        }
-        cnt = query[i] === info[i] ? cnt + 1 : cnt;
-      }
-      console.log(cnt);
-      if (cnt === 5) {
-        total += 1;
-      }
-    });
-    answer.push(total);
-  });
-
-  console.log(answer);
-
-  return answer;
+	info.forEach(el => infoArr.push(el.split(' ')));
+	query.forEach(el => {
+			const splited = el.split(' and ');
+			splited.push(...splited.pop().split(' '));
+			queryArr.push(splited);
+	})
+	infoArr.sort((a,b) => b[4] - a[4]);
+	
+	queryArr.forEach(query => {
+			let total = 0;
+			for(let z=0; z < infoArr.length; z++){
+					let cnt = 0;
+					if(parseInt(infoArr[z][4], 10) >= parseInt(query[4], 10)){
+							for(let i = 0; i < 4; i ++) {
+									if(cnt !== i) break;
+									if(query[i] === '-') cnt += 1;
+									else cnt = query[i] === infoArr[z][i] ? (cnt + 1) : cnt;  
+							}
+					// console.log(`[${query}]\t[${infoArr[z]}]\t${cnt}`)
+					if(cnt === 4) total += 1;
+					} else {
+							break;
+					}
+			}
+			answer.push(total);
+	})
+	return answer;
 }
+
+/*
+	검색 조건에 대한 dic 만들어주기
+	x점 맞은 사람을 찾을 때 binary search 이용하기 
+	찾으면 비교후 카운트
+*/
