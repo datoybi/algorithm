c0ab49dbd80ce31331fd6a1e954a30729ab612a1
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-01-03 17:12:30 +0900

    구현 문제 푸는 중

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /*
-	https://www.acmicpc.net/problem/17413
-	단어 뒤집기 2
-
+	https://www.acmicpc.net/problem/15787
+	기차가 어둠을 헤치고 은하수를
 	구현문제
 */
 
@@ -12,26 +11,4 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-const plainReverse = (arr) =>
-  arr.map((el, i) => el.split("").reverse().join(""));
-
-const reverse = (arr) =>
-  arr.map((el, i) => {
-    if (i % 2 === 0) return plainReverse(el.split(" ")).join(" ");
-    else return `<${el}>`;
-  });
-
-let arr = [];
-let result = "";
-
-const isPlain = !input[0].includes("<");
-
-if (isPlain) {
-  arr = input[0].split(" ");
-  result = plainReverse(arr);
-  console.log(result.join(" "));
-} else {
-  arr = input[0].split(/<|>/);
-  result = reverse(arr);
-  console.log(result.join(""));
-}
+console.log(input);
